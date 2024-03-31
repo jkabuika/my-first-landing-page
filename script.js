ad3ef2af8de2e236bc878df4f4ecd65f24e08203
jkabuika <jkabuika1319@gmail.com>
2024-03-31 15:11:44 -0400

    Changed the JS a bit

--- script.js
+++ script.js
@@ -13,18 +13,17 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-// Got the navigation bar & the offset position of the navbar
-const navbar = document.getElementById("navbar");
-const sticky = navbar.offsetTop;
+document.addEventListener("DOMContentLoaded", function() {
+  const navbar = document.getElementById("navbar");
+  const sticky = navbar.offsetTop;
 
-// Added the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickyNavbar() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
+  function stickyNavbar() {
+      if (window.pageYOffset >= sticky) {
+          navbar.classList.add("sticky");
+      } else {
+          navbar.classList.remove("sticky");
+      }
   }
-}
 
-// Listen for the scroll event
-window.addEventListener("scroll", stickyNavbar);
+  window.addEventListener("scroll", stickyNavbar);
+});
